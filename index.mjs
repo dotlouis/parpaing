dd7ee37f0a34b10b8708a337fc86edebd8e38d4c
dotlouis <1891109+dotlouis@users.noreply.github.com>
2018-09-17 15:23:18 +0200

    Moved firebase credential from json file to env variables

--- index.mjs
+++ index.mjs
@@ -2,7 +2,6 @@ import SlackClient from '@slack/client';
 import firebase from 'firebase-admin';
 import micro from 'micro';
 import parse from 'urlencoded-body-parser';
-import serviceAccount from './parpaing-e3e5c-firebase-adminsdk-sxqzn-23c4cf2924.json';
 import microrouter from 'microrouter';
 import crypto from 'crypto';
 import schedule from 'node-schedule';
@@ -13,8 +12,21 @@ const { WebClient } = SlackClient;
 const slack = new WebClient(process.env.SLACK_TOKEN);
 
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: 'https://parpaing-e3e5c.firebaseio.com',
+  credential: firebase.credential.cert({
+    type: `service_account`,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: `https://accounts.google.com/o/oauth2/auth`,
+    token_uri: `https://oauth2.googleapis.com/token`,
+    auth_provider_x509_cert_url: `https://www.googleapis.com/oauth2/v1/certs`,
+    client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${encodeURIComponent(
+      process.env.FIREBASE_CLIENT_EMAIL,
+    )}`,
+  }),
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
 });
 
 const store = firebase.firestore();
